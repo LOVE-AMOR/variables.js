a6498ecb69054d0374d9eaaedef39bf7a963efc0
LOVE-AMOR <loveamorbalaqui2@gmail.com>
2024-09-13 20:02:44 -0800

    Iniial commit

---
+++ variables.js
@@ -0,0 +1,11 @@
+let name = "Love Amor J. Balaqui";
+console.log("Name:", name);
+
+let WhatDoYouWannaBecomeInYourLife = "Programmer";
+console.log("Desired Career:", WhatDoYouWannaBecomeInYourLife);
+
+let gender = "male";
+console.log("Gender:", gender);
+
+let twitterHandle = "@amor_balaqui";
+console.log("Twitter Handle:", twitterHandle);
